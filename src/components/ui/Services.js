43a003dd8494f04868930516e53027302e42d7fa
Jaime Rios <jaime.rios.dev@outlook.com>
2021-05-18 17:44:08 -0500

    Update Services.js

--- src/components/ui/Services.js
+++ src/components/ui/Services.js
@@ -4,44 +4,50 @@ import Popup from "./Popup";
 import { ServiceData } from "./ServicesData";
 
 const Services = () => {
-	const [isOpen, setIsOpen] = useState(false);
-	const [content, setContenxt] = useState();
+  const [isOpen, setIsOpen] = useState(false);
+	const [content, setContent] = useState();
+  const togglePopup = (index) => {
+	  if(isOpen) {
+		  setIsOpen(!isOpen)	
+		return
+	  }
+	setIsOpen(!isOpen)	
 
-	const togglePopup = (index) => {
-		setIsOpen(!isOpen);
-	const	setContent(ServiceData[index].description)
-	};
-	return (
-		<div className="main-container">
-			{ServiceData.map((icon, index) => {
-				return (
-					<div
-						className={`service-container container-${index}`}
-						onClick={togglePopup}
-						key={index}
-					>
-						<div className="titles-container">
-							<div className="icons">{icon.icon}</div>
-							<h2 className="title">{icon.title}</h2>
-							<h4 className="subtitle">{icon.titleTwo}</h4>
-						</div>
+	console.log(ServiceData)
+	debugger
+	setContent(ServiceData[index].description);
+  };
+  return (
+    <div className="main-container">
+      {ServiceData.map((icon, index) => {
+        return (
+          <div
+            className={`service-container container-${index}`}
+            onClick={() => togglePopup(index)}
+            key={index}
+          >
+            <div className="titles-container">
+              <div className="icons">{icon.icon}</div>
+              <h2 className="title">{icon.title}</h2>
+              <h4 className="subtitle">{icon.titleTwo}</h4>
+            </div>
 
-						<a href={icon.link} target="_blank" className="link">
-							{icon.linkName}
-						</a>
+            <a href={icon.link} target="_blank" className="link">
+              {icon.linkName}
+            </a>
 
-						{isOpen && (
-							<Popup key={}
-								content={icon.description}
-								handleClose={() => togglePopup(index)}
-							/>
-						)}
-						<hr />
-					</div>
-				);
-			})}
-		</div>
-	);
+            {isOpen && content && (
+              <Popup
+                content={content}
+                handleClose={togglePopup}
+              />
+            )}
+            <hr />
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Services;
